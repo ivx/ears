8d1c02f2b5c1eb5543777c72a7a6b9490e9363dc
Thomas Müller <thomas.mueller@invision.de>
2023-10-25 10:26:38 +0200

    Add worker configuration interface

--- spec/ears/consumer_spec.rb
+++ spec/ears/consumer_spec.rb
@@ -16,6 +16,124 @@ RSpec.describe Ears::Consumer do
     allow(channel).to receive(:ack).with(delivery_tag, false)
   end
 
+  describe '.configure' do
+    let(:mandatory_options) do
+      { queue: 'test', exchange: 'exchange', routing_key: 'test.new' }
+    end
+    let(:custom_consumer_class) { Class.new(Ears::Consumer) }
+
+    it 'sets the queue' do
+      custom_consumer_class.configure(mandatory_options)
+
+      expect(custom_consumer_class.queue).to eq('test')
+    end
+
+    it 'sets the exchange' do
+      custom_consumer_class.configure(mandatory_options)
+
+      expect(custom_consumer_class.exchange).to eq('exchange')
+    end
+
+    it 'sets the routing key' do
+      custom_consumer_class.configure(mandatory_options)
+
+      expect(custom_consumer_class.routing_key).to eq('test.new')
+    end
+
+    context 'without providing specific options' do
+      it 'provides defaults for queue options' do
+        custom_consumer_class.configure(mandatory_options)
+
+        expect(custom_consumer_class.queue_options).to eq(
+          {
+            durable: true,
+            retry_queue: false,
+            retry_delay: 5000,
+            error_queue: false,
+          },
+        )
+      end
+
+      it 'provides a default for durable_exchange' do
+        custom_consumer_class.configure(mandatory_options)
+
+        expect(custom_consumer_class.durable_exchange).to eq(true)
+      end
+
+      it 'provides a default for exchange_type' do
+        custom_consumer_class.configure(mandatory_options)
+
+        expect(custom_consumer_class.exchange_type).to eq(:topic)
+      end
+    end
+
+    context 'with durable_queue specified' do
+      it 'sets the queue options' do
+        custom_consumer_class.configure(
+          mandatory_options.merge({ durable_queue: false }),
+        )
+
+        expect(custom_consumer_class.queue_options).to include(durable: false)
+      end
+    end
+
+    context 'with retry_queue specified' do
+      it 'sets the queue options' do
+        custom_consumer_class.configure(
+          mandatory_options.merge({ retry_queue: true }),
+        )
+
+        expect(custom_consumer_class.queue_options).to include(
+          retry_queue: true,
+        )
+      end
+    end
+
+    context 'with retry_delay specified' do
+      it 'sets the queue options' do
+        custom_consumer_class.configure(
+          mandatory_options.merge({ retry_delay: 1000 }),
+        )
+
+        expect(custom_consumer_class.queue_options).to include(
+          retry_delay: 1000,
+        )
+      end
+    end
+
+    context 'with error_queue specified' do
+      it 'sets the queue options' do
+        custom_consumer_class.configure(
+          mandatory_options.merge({ error_queue: true }),
+        )
+
+        expect(custom_consumer_class.queue_options).to include(
+          error_queue: true,
+        )
+      end
+    end
+
+    context 'with durable_exchange specified' do
+      it 'sets the durable_exchange' do
+        custom_consumer_class.configure(
+          mandatory_options.merge({ durable_exchange: false }),
+        )
+
+        expect(custom_consumer_class.durable_exchange).to eq(false)
+      end
+    end
+
+    context 'with exchange_type specified' do
+      it 'sets the exchange_type' do
+        custom_consumer_class.configure(
+          mandatory_options.merge({ exchange_type: :direct }),
+        )
+
+        expect(custom_consumer_class.exchange_type).to eq(:direct)
+      end
+    end
+  end
+
   describe '#work' do
     it 'raises a not implemented error' do
       expect { instance.work(delivery_info, metadata, payload) }.to raise_error(
