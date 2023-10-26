8a5b64416fe942389a5ce46d2a4c26175b0a1fb7
Johannes Lüdke <johannes.luedke@invision.de>
2023-10-26 12:29:35 +0200

    Start simplecov early

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require 'rspec/core'
 require 'simplecov'
-require 'ears'
-
 SimpleCov.start do
   enable_coverage :branch
   primary_coverage :branch
 end
 
+require 'rspec/core'
+require 'ears'
+
 RSpec.configure(&:disable_monkey_patching!)
 
 if RSpec.configuration.files_to_run.length > 1
   # Let's increase this later on
-  SimpleCov.minimum_coverage line: 99.4, branch: 100
+  SimpleCov.minimum_coverage line: 97.4, branch: 62.8
 end
